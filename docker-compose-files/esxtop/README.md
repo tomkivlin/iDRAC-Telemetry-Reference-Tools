8a6185e150faef58da81ef681485c0ceed9edb75
Tom Kivlin <tom.kivlin@vodafone.com>
2022-10-07 08:27:01 +0000

    big save
    Signed-off-by: Tom Kivlin <tom.kivlin@vodafone.com>

---
+++ docker-compose-files/esxtop/README.md
@@ -0,0 +1,87 @@
+# Overview 
+
+`esxtop` is able to record data in a `batch mode`.  In `batch mode` data is stored in a time-stamped csv format.  Reviewing the data can be difficult due to the format, quantity, and a lack of tools that can quickly consume it.  This project consumes this CSV data and structures it in to prometheus query results.  This project is intended to be 'turn-key' and stands up an instance of grafana and the metric server.  Data is consumed from a file named `metrics.csv` which is to be placed in `dataserv/data/esxtop/metrics.csv`.
+
+# Requirements
+
+- podman
+- [podman-compose](https://github.com/containers/podman-compose)
+- ssh access to an ESXi host
+
+# Gathering Data
+
+In its most basic invocation, `esxtop` will record all known metrics:
+
+~~~
+ssh <esxuser>@<esxihost> esxtop -b &> metrics.csv
+~~~
+
+This is usually a staggering number of metrics and can be difficult to effectively parse.  It is recommended that metrics are selectively chosen to reduce the burden on the collection and review of the metrics.  To do this:
+
+1. Determine the entities of the metrics being monitored
+
+~~~
+ssh <esxuser>@<esxihost> esxtop -export-entity entities.dat
+scp <esxuser>@<esxihost>:entities.dat .
+~~~
+
+In this file there are entity groups.  `SchedGroup` contains processes and VMs.  If the goal is to collect metrics from VMs associated with nodes in the cluster the VMs of interest must be isolated.
+
+2. Filter entities to collect
+
+~~~
+NODES=$(oc get nodes -o=jsonpath='{.items[*].metadata.name}')
+echo SchedGroup > filtered.dat
+for NODE in $NODES; do cat entities.dat | grep $NODE >> filtered.dat ; done
+scp filtered.dat <esxuser>@<esxihost>: .
+~~~
+
+3. Collect metrics
+
+~~~
+ssh <esxuser>@<esxihost> esxtop -b -import-entity filtered.dat | tee metrics.csv
+~~~
+
+When finished, `control+c` can be invoked.  Also, `esxtop` accepts parameters which define the time between collections(`-d`) and the number of collections(`-n`).  Otherwise, `esxtop` will run until interrupted.
+
+4. Start Metrics Server
+
+a. Copy `metrics.csv` to `dataserv/data/esxtop/metrics.csv`.  
+b. From the root of the project, run `podman-compose build;podman-compose up`
+
+
+5. Analyze Metrics
+
+Enter `http://localhost:3000` in to your browser of choice.  The datasource is `esxtop batch reader`.  The metrics server only has limited query support:
+- The timeframe of the metrics can be restricted
+- Indiviual metrics can be selected for query
+- Substrings of metrics can be provided in the query
+
+
+# Overlaying Additional Metrics
+
+Metrics may also be directly exported from prometheus to augment the data gathered from `esxtop`.  This can be helpful, if for example, it is needed to
+correlate etcd performance against ESXi performance.  
+
+## Extracting Metrics From Prometheus
+
+~~~
+function promqlQuery() {
+    END_TIME=$(date -u +%s) 
+    START_TIME=$(date -u --date="60 minutes ago" +%s)
+
+    oc exec -c prometheus -n openshift-monitoring prometheus-k8s-0 -- curl --data-urlencode "query=$1" --data-urlencode "step=10" --data-urlencode "start=$START_TIME" --data-urlencode "end=$END_TIME" http://localhost:9090/api/v1/query_range 
+}
+
+promqlQuery "histogram_quantile(0.99, sum(rate(etcd_disk_backend_commit_duration_seconds_bucket{job=\"etcd\"}[1m])) by (instance, le))" > etcd_disk_backend_commit_duration_seconds_bucket_.99
+promqlQuery "histogram_quantile(0.999, sum(rate(etcd_disk_backend_commit_duration_seconds_bucket{job=\"etcd\"}[1m])) by (instance, le))" > etcd_disk_backend_commit_duration_seconds_bucket_.999
+promqlQuery "histogram_quantile(0.9999, sum(rate(etcd_disk_backend_commit_duration_seconds_bucket{job=\"etcd\"}[1m])) by (instance, le))" > etcd_disk_backend_commit_duration_seconds_bucket_.9999
+~~~
+
+## Adding Extracted Metrics to Grafana
+
+Copy the extracted metrics file to `dataserv/data/promql`.  The files will be read in under the `promql result reader` data source in grafana.
+
+## Processing etcd log files
+
+etcd log files can be read without any required preprocessing by placing them in `dataserv/data/etcd`.  There will be an etcd datasource when podman-compose is started.
---
+++ docker-compose-files/esxtop/dataserv/esxtop_metric_server.py
@@ -0,0 +1,173 @@
+
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
+from datetime import datetime
+from flask import Flask, jsonify, request, Blueprint
+import json 
+import copy
+import re
+from os import listdir
+from os.path import isfile, join
+
+
+esxtop_bp = Blueprint('esxtop', __name__)
+
+columnMap = dict()
+metrics = dict()
+hostname=""
+
+print(">>>> Initializing esxtop datasource")
+
+@esxtop_bp.route("/esxtop/api/v1/label/__name__/values", methods=['GET'])
+def labels():
+    #reloadMetrics()
+    labels = []
+    outResults = {
+        "status": "success",
+        "data": labels
+    }
+    for col in columnMap:
+        if col == "time":
+            continue
+        labels.append(col)
+    return json.dumps(outResults)
+    
+@esxtop_bp.route("/esxtop/api/v1/values", methods=['GET'])
+@esxtop_bp.route("/esxtop/api/v1/query", methods=['GET'])
+@esxtop_bp.route("/esxtop/api/v1/query_range", methods=['GET'])
+def query():
+    # reloadMetrics()
+    query_parameters = request.args
+    
+    query_val = query_parameters.get('query')
+    start_time = query_parameters.get('start')
+    end_time = query_parameters.get('end')
+    return populateResults(query_val,start_time,end_time)
+    
+
+@esxtop_bp.route("/esxtop/api/v1/metadata", methods=['GET'])
+def metadata():
+    return getMetadata()
+
+
+metadataResults = {
+    "status": "success",
+    "data": {
+    }
+}
+
+results = {
+    "status": "success",
+    "data": {
+        "resultType": "matrix",
+        "result": [
+
+        ]
+    }
+}
+
+result = {
+    "metric": {
+        "__name__":"",
+        "job": "",
+        "instance": ""
+    },
+    "values": []
+}
+
+def getMetadata():
+    outResults = copy.deepcopy(metadataResults)
+    for col in columnMap:
+        if col == "time":
+            continue
+        outResults["data"][col] = [{
+            "type": "gauge",
+            "help": "",
+            "unit": ""
+        }]
+        
+    return json.dumps(outResults)
+
+def populateResults(query, start, end):
+    outResults = copy.deepcopy(results)
+    resultMap = {}
+    
+    for col in columnMap:        
+        if col == "time" or (query != None and col.find(query) == -1):
+            continue
+        thisResult = copy.deepcopy(result)
+        resultMap[col] = thisResult
+        thisResult["metric"]["__name__"] = col    
+        thisResult["metric"]["instance"] = hostname
+
+    index = 0
+
+    for t in metrics["time"]:
+        if (start != None and (t < float(start))) or (end != None and (t > float(end)) ):
+            index = index + 1
+        else:
+            for col in columnMap:            
+                if col == "time" or (query != None and col.find(query) == -1):
+                    continue                
+                resultMap[col]["values"].append([t,metrics[col][index]])
+            index = index + 1    
+        
+
+    for col in columnMap:
+        if col in resultMap:
+            outResults["data"]["result"].append(resultMap[col])    
+    
+    return json.dumps(outResults)
+
+def timeToMillis(time):    
+    return datetime.strptime(time,'%m/%d/%Y %H:%M:%S').timestamp()
+
+def reloadMetrics():
+    global hostname, columnMap, metrics
+    columnMap = dict()
+    metrics = dict()
+
+    for fname in listdir('/csv/data/esxtop'):
+        fullPath = join('/csv/data/esxtop',fname)
+        if isfile(fullPath) and fname[0] != "." :
+            print(">>>> Loading " + fname)
+            with open(fullPath) as f:    
+                print(">>>> Opened " + fname)
+                firstLine = True
+                for line in f:
+                    columns = line.split(",")        
+                    index = 0
+                    if firstLine:
+                        firstLine = False
+                        metrics["time"] = []
+                        for col in columns:
+                            try:
+                                col = col[3:]
+                                #try: 
+                                hostnameLoc = col.index("\\")   
+                                if hostname == "":
+                                    hostname = col[0:hostnameLoc]           
+                                    print("hn:"+hostname)      
+                                name = col[hostnameLoc+1:-1]
+                                name = re.sub("[() ]","",name).replace('\\', '_')                    
+                                # except ValueError:
+                                #     name = fname+"_"+col
+                                #     name = re.sub("[() ]","",name).replace('\\', '_')                    
+                                columnMap[name] = index
+                                metrics[name] = []
+                                index = index + 1
+                            except ValueError:
+                                index = index + 1
+                                continue                 
+                    else:
+                        metrics["time"].append(timeToMillis(columns[0].replace('"', '')))
+                        for col in columnMap:
+                            val = columns[columnMap[col]].replace('"', '')
+                            metrics[col].append(val)
+
+print(">>>> Initialized esxtop datasource")
+
+reloadMetrics()
+
+
